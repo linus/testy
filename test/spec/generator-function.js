e71ba06bad39db5b8f471f93feebcd1b335c30ea
Linus Thiel <linus@yesbabyyes.se>
2023-01-05 22:39:54 +0100

    Rename function

--- test/spec/generator-function.js
+++ test/spec/generator-function.js
@@ -2,25 +2,25 @@
  * Generator functions can be tested in a number of ways
  *
  * @template T
- * @param {T[]} array
+ * @param {T[]} values
  * @yields T
- * @example generatorFunction(["foo", "bar", "baz"]).next()
+ * @example generateValues(["foo", "bar", "baz"]).next()
  * // Call .next() immediately to check the first value
  * //=> {
  *   value: "foo",
  *   done: false
  * }
- * @example Array.from(generatorFunction([42, 17]))
+ * @example Array.from(generateValues([42, 17]))
  * // Consume the iterator into an array, and test that
  * //=> [42, 17]
  * @example
  * // Assign the iterator to a variable and iterate as many steps as needed
- * const it = generatorFunction(["baz", "qux"]);
+ * const it = generateValues(["baz", "qux"]);
  * it.next();
  * it.next();
  * it.next();
  * //=> {
-     value: undefined,
+ *   value: undefined,
  *   done: true
  * }
  */
