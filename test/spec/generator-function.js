d3c84b839697a4e72de485facec4cf745b39c48f
Linus Thiel <linus@yesbabyyes.se>
2023-01-05 22:40:16 +0100

    Add fibonacci to generator spec

--- test/spec/generator-function.js
+++ test/spec/generator-function.js
@@ -24,6 +24,41 @@
  *   done: true
  * }
  */
-export function* generatorFunction(array) {
-  for (const x of array) yield x;
+export function* generateValues(values) {
+  for (const x of values) yield x;
+}
+
+/**
+ * Here's old Fibonacci
+ *
+ * @param {number[]} start
+ * @yields {number}
+ * @example fibonacci().next().value
+ * //=> 0
+ * @example
+ * const f = fibonacci();
+ * f.next();
+ * f.next().value
+ * //=> 1
+ * @example
+ * const f = fibonacci();
+ * f.next();
+ * f.next();
+ * f.next().value
+ * //=> 1
+ * @example
+ * const f = fibonacci();
+ * [...Array(13)].map(() => f.next().value)
+ * //=> [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+ * @example
+ * const f = fibonacci([8, 13]);
+ * [...Array(13)].map(() => f.next().value)
+ * //=> [8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584]
+ */
+export function* fibonacci([last, next] = [0, 1]) {
+  yield last;
+  while (true) {
+    yield next;
+    [last, next] = [next, next + last];
+  }
 }
